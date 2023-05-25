ee2b51f84e12523b0727a48f21d473546034bf6f
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-05-25 19:14:51 +0200

    Added "tasks" Array and render()

--- js/script.js
+++ js/script.js
@@ -3,8 +3,25 @@
         console.log("Hello World!");
     };
 
+    const tasks = [];
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+                ${task.content}
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+    };
+
     const init = () => {
         welcome();
+        render();
     };
 
     init();
